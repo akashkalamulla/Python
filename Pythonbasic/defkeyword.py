0df3aed98be868fa0e9fc3911a3ccaf45f1cba0e
Akash kalamulla <akashkalamulla1@gmail.com>
2023-12-23 23:39:39 +0530

    commit

---
+++ Pythonbasic/defkeyword.py
@@ -0,0 +1,10 @@
+class Puppy():
+  def __init__(self, name, favourite_toy):
+    self.name = name
+    self.favourite_toy = favourite_toy
+
+  def play(self):
+    print(self.name + " is playing with the " + self.favourite_toy)
+
+marble = Puppy('Marble','teddy bear')
+marble.play()
